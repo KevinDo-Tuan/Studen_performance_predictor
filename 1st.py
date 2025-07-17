6b82a1e9aadaa8bae306253bd7ebfd121a888772
tuan <dophamtuan2008@gmail.com>
2025-07-17 17:36:51 +0700

    changes in 07/17/2026 5:37 PM

--- 1st.py
+++ 1st.py
@@ -3,10 +3,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+# data 
 data = pd.read_csv(r"C:\Users\Do Pham Tuan\.cache\kagglehub\datasets\neuralsorcerer\student-performance\versions\1\test.csv")
 data1 =data.head(500)
+data2 =data[500:1000]
+data3 =data[1000:1500]
+data4 =data[1500:2000]
+data5 = data[2000:2500]
+data6 = data[2500:3000]
 
+# Gender
+gender = {
+    "Female" :0,
+    "Male" :1,
+
+}
+data["gender"] = data["gender"].map(gender)
+
+#Race
+race = {
+    "Black": 0,
+    "White": 1,
+    "Hispanic": 2,
+    "Two-or-more": 3,
+    "Other": 4,}
+data ["race"] = data["race"].map(race)
 def chat_showdata():
     print ("can you describe your personality in 1 word so that I can understand you better, thank you?")
     a = input ("")
@@ -26,12 +47,17 @@ def chat_showdata():
             print("Here is the dataset:")
             print(data)
             
-            sns.pairplot(data)  # start changing the dataset to plot it on sns
+            sns.pairplot(data1) 
+            sns.pairplot(data2)
+            sns.pairplot(data3)
+            sns.pairplot(data4)
+            sns.pairplot(data5)
+            sns.pairplot(data6)      # start changing the dataset to plot it on sns
             plt.show()
     
 chat_showdata()
 
-def predicting():
+
 
 
 
