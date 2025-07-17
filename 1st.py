da9c072420716d06c4e8b7407853340f23f2880c
tuan <dophamtuan2008@gmail.com>
2025-07-17 17:16:22 +0700

    change 17/07 5:16

--- 1st.py
+++ 1st.py
@@ -1,33 +1,38 @@
 import kagglehub as kg
 import pandas as pd
 import seaborn as sns
-print ("can you describe your personality in 1 word so that I can understand you better, thank you?")
-g = input ("")
+import matplotlib.pyplot as plt
 
-if g is not float:
-    print("Do you want to predict early signs of diabetes?")
-else:
-    print( "please enter a valid word")
 
-chat = input("")
+data = pd.read_csv(r"C:\Users\Do Pham Tuan\.cache\kagglehub\datasets\neuralsorcerer\student-performance\versions\1\test.csv")
+data = data.head(500)
 
-kg.dataset_download("alexteboul/diabetes-health-indicators-dataset")
+def chat_showdata():
+    print ("can you describe your personality in 1 word so that I can understand you better, thank you?")
+    a = input ("")
 
-file = pd.read_csv(r"c:\Users\Do Pham Tuan\.cache\kagglehub\datasets\alexteboul\diabetes-health-indicators-dataset\versions\1\diabetes_012_health_indicators_BRFSS2015.csv")
-transform = chat.lower()
-
-for i in transform:
-    if i == "y":
-        print("great, let's go.")
-        print ("do you first want to see the data?")
-        input("")
-        for i in input(""):
-            if i == "y":
-                print(file)
-                sns.pairplot(file)
-            else: print("ok, let's continue")
-
+    if a is not float:
+        print("Do you want to predict student performance?")
     else:
-        print("ok, have a nice day though")
+        print( "please enter a valid word")
+    b = input("")
+    
+    transform = b.lower()
+    
+    if transform[0] == "y":
+        print("start with the datset?")
+        c = input("")
+        if c.lower()[0] == "y": 
+            print("Here is the dataset:")
+            print(data)
+            
+            sns.pairplot(data)  # start changing the dataset to plot it on sns
+            plt.show()
+    
+chat_showdata()
+
+def predicting():
+
+
 
 
---
+++ download_dataset.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("neuralsorcerer/student-performance")
+
+print("Path to dataset files:", path)
