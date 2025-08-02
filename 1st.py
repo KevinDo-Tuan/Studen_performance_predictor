f4ffe7dac40fa6dbd6bde6ba24ad3b1b61aa2066
tuan <dophamtuan2008@gmail.com>
2025-08-03 00:53:19 +0700

    new

--- 1st.py
+++ 1st.py
@@ -70,12 +70,14 @@ def check_data():
 check_data() 
 
 def finding_parameters(trial):
-    hidden_layer1 = trial.suggest_int("hidden_layer_sizes", 10, 50)
-    hidden_layer2 = trial.suggest_int("hidden_layer_sizes2", 10, 50)
-    max_iter = trial.suggest_int("max_iter", 10, 50)
-    random_state = trial.suggest_int("random_state", 10, 50)
+    hidden_layer1 = trial.suggest_int("hidden_layer1", 10, 100)
+    hidden_layer2 = trial.suggest_int("hidden_layer2", 10, 100)
+    hiden_layer3 = trial.suggest_int("hidden_layer3", 10, 100)
+    hidden_layer4 = trial.suggest_int("hidden_layer4", 10, 100)
+    max_iter = trial.suggest_int("max_iter", 10, 200)
+    random_state = trial.suggest_int("random_state", 10, 100)
 
-    model = MLPRegressor(hidden_layer_sizes=(hidden_layer1, hidden_layer2), max_iter=max_iter, random_state=random_state)
+    model = MLPRegressor(hidden_layer_sizes=(hidden_layer1, hidden_layer2, hiden_layer3, hidden_layer4), max_iter=max_iter, random_state=random_state)
     model.fit(X_train, Y_train)
     v = model.predict(X_test)
     r2 = r2_score(Y_test, v)
@@ -84,12 +86,17 @@ def finding_parameters(trial):
 
 print ("finding parameters...please wait")
 study = optu.create_study(direction="maximize")
-study.optimize(finding_parameters, n_trials=30)
+study.optimize(finding_parameters, n_trials=3)
 
 print("Best parameters found:", study.best_params)
-
+    
 def train_model():
-    model = MLPRegressor(study.best_params['hidden_layer_sizes'],
+    model = MLPRegressor(hidden_layers = (
+    study.best_params['hidden_layer_1'],
+    study.best_params['hidden_layer_2'],
+    study.best_params['hidden_layer_3'],
+    study.best_params['hidden_layer_4']
+),
                          max_iter=study.best_params['max_iter'],
                          random_state=study.best_params['random_state'])
     
